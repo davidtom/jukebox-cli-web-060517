681e9c8d2b29bd8f54863195b8550e6c68eaae42
davidtom <davidjtomczyk@gmail.com>
2017-05-28 11:41:24 -0400

    completed first simple jukebox. will return to complete advanced jukebox

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,4 +1,5 @@
-songs = [
+def songs
+  [
   "Phoenix - 1901",
   "Tokyo Police Club - Wait Up",
   "Sufjan Stevens - Too Much",
@@ -8,5 +9,65 @@ songs = [
   "Phoenix - Consolation Prizes",
   "Harry Chapin - Cats in the Cradle",
   "Amos Lee - Keep It Loose, Keep It Tight"
-]
+  ]
+end
 
+def help
+  puts <<-TEXT
+  I accept the following commands:
+- help : displays this help message
+- list : displays a list of songs you can play
+- play : lets you choose a song to play
+- exit : exits this program
+  TEXT
+end
+
+def list(songs)
+  songs.each_with_index { |song, index|
+    puts "#{index+1}. #{song}"
+  }
+end
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  song = gets.chomp
+
+  valid_song_numbers = (1..songs.length).to_a
+
+  if songs.include?(song)
+    puts "Playing #{song}"
+  elsif valid_song_numbers.include?(song.to_i)
+    puts "playing #{songs[song.to_i - 1]}"
+  else
+    puts "Invalid input, please try again"
+  end
+
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def run(songs)
+  help
+
+  command = String.new
+
+  while true
+    puts "Please enter a command:"
+    command = gets.chomp
+
+    case command
+    when "help"
+      help
+    when "list"
+      list(songs)
+    when "play"
+      play(songs)
+    when "exit"
+      exit_jukebox
+      break
+    end
+  end
+
+end
